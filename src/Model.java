40071a0322f0e78f43ca2b243ab0eafc6cc57bd7
Egor Romanovskiy <37875413+mayotte203@users.noreply.github.com>
2019-03-01 08:43:39 +0300

    Add files via upload

---
+++ src/Model.java
@@ -0,0 +1,45 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class Model {
+    public String computeSquare(String widthString, String heightString)
+    {
+        int width = 0;
+        int height = 0;
+        try {
+            width = Integer.parseInt(heightString);
+            height = Integer.parseInt(widthString);
+        }
+        catch (NumberFormatException e)
+        {
+            width = 0;
+            height = 0;
+        }
+        if(width < 0 || height < 0)
+        {
+            width = 0;
+            height = 0;
+        }
+        return "" + (width * height);
+    }
+    public String computePerimeter(String widthString, String heightString)
+    {
+        int width = 0;
+        int height = 0;
+        try {
+            width = Integer.parseInt(heightString);
+            height = Integer.parseInt(widthString);
+        }
+        catch (NumberFormatException e)
+        {
+            width = 0;
+            height = 0;
+        }
+        if(width < 0 || height < 0)
+        {
+            width = 0;
+            height = 0;
+        }
+        return "" + 2 * (width + height);
+    }
+}
